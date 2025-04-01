857a1b55e2c9cd6463bbc2f3dfc69b4443971821
fati <fatemesadatsedigh@gmail.com>
2025-04-01 18:07:57 +0330

    writing-public static void add(Entity e)

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,4 +1,20 @@
 package db;
 
+import java.util.ArrayList;
+import db.exception.EntityNotFoundException;
+
 public class Database {
+
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database() {}
+
+    public static void add(Entity e){
+        e.id = nextId++;
+        entities.add(e);
+    }
+
+
+
 }
