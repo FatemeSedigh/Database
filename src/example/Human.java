9e1871e5add93ddc72660133e0bb9b17b31d499c
fati <fatemesadatsedigh@gmail.com>
2025-04-03 12:50:31 +0330

    add public int getEntityCode() in class Human

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,19 +3,24 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
-    public int age;
+    public int age; // فیلد جدید age
 
-    public Human(String name,int age) {
+    public Human(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
     @Override
     public Human copy() {
-        Human copyHuman = new Human(this.name, this.age);
-        copyHuman.id = this.id;
-
-        return copyHuman;
+        Human copy = new Human(this.name, this.age);
+        copy.id = this.id;
+        return copy;
     }
 
+    public static final int HUMAN_ENTITY_CODE = 14;
+
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
+    }
 }
