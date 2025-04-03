ca0d3492813ba17575879b406bda1608c56370e1
fati <fatemesadatsedigh@gmail.com>
2025-04-03 11:41:10 +0330

    add age

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,15 +3,17 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name,int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Override
     public Human copy() {
-        Human copyHuman = new Human(name);
-        copyHuman.id = id;
+        Human copyHuman = new Human(this.name, this.age);
+        copyHuman.id = this.id;
 
         return copyHuman;
     }
