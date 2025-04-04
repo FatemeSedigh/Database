4e244424673ef4c7b7446486bd880690de742674
fati <fatemesadatsedigh@gmail.com>
2025-04-04 10:57:53 +0330

    add fields class Document

--- src/example/Document.java
+++ src/example/Document.java
@@ -2,7 +2,18 @@ package example;
 
 import db.Entity;
 import db.Trackable;
+import java.util.Date;
 
 public class Document extends Entity implements Trackable {
+
+    private String content;
+    private Date creationDate;
+    private Date lastModificationDate;
+
+    public Document(String content) {
+        this.content = content;
+    }
+
+
 
 }
