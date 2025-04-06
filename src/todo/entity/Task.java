d1872cb9ceff0581c5dd0c9aaa2be0e2c8d0b4ec
fati <fatemesadatsedigh@gmail.com>
2025-04-06 17:02:57 +0330

    add getter and setter

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -29,5 +29,15 @@ public class Task extends Entity implements Trackable {
     @Override public void setLastModificationDate(Date date) { this.lastModificationDate = date; }
     @Override public Date getLastModificationDate() { return lastModificationDate; }
 
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public Date getDueDate() { return dueDate; }
+    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+
 
 }
