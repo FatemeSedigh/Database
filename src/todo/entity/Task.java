e76e2982688e1c77582ce1725d0f4ddd59afe3e7
fati <fatemesadatsedigh@gmail.com>
2025-04-06 16:57:03 +0330

    add fields

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -1,11 +1,19 @@
 package todo.entity;
 
 import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
 
 public class Task extends Entity implements Trackable {
     public enum Status {
         NotStarted, InProgress , Completed
     }
+
+    public String title;
+    public String description;
+    public Date dueDate;
+    private Status status;
 
 
 
