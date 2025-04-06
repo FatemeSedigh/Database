5e66493670f91b91810a4825bacad71a120d7aa8
fati <fatemesadatsedigh@gmail.com>
2025-04-06 17:40:02 +0330

    add createTask

--- src/todo/validator/TaskService.java
+++ src/todo/validator/TaskService.java
@@ -1,12 +1,20 @@
 package todo.validator;
 
 import db.Database;
+import db.exception.InvalidEntityException;
+import todo.entity.Task;
 import todo.service.TaskValidator;
 
 public class TaskService {
 
     static {
         Database.registerValidator(1, new TaskValidator());
+    }
+
+    public static void createTask(String title, String description, Date dueDate)
+            throws InvalidEntityException {
+        Task task = new Task(title, description, dueDate);
+        Database.add(task);
     }
 
 
