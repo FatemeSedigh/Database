e7607cf2ad5ec1d823d7d5564c1e6fc6c0092cc8
fati <fatemesadatsedigh@gmail.com>
2025-04-06 17:39:06 +0330

    add static

--- src/todo/validator/TaskService.java
+++ src/todo/validator/TaskService.java
@@ -1,4 +1,14 @@
 package todo.validator;
 
+import db.Database;
+import todo.service.TaskValidator;
+
 public class TaskService {
+
+    static {
+        Database.registerValidator(1, new TaskValidator());
+    }
+
+
+
 }
