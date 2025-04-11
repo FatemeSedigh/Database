4f59fe671ef8b31f4a474b1d3445c4b7ad577722
fati <fatemesadatsedigh@gmail.com>
2025-04-11 11:12:29 +0330

    add static

--- src/todo/validator/StepService.java
+++ src/todo/validator/StepService.java
@@ -1,4 +1,14 @@
 package todo.validator;
 
+import db.Database;
+import todo.entity.Step;
+
 public class StepService {
+
+    static {
+        Database.registerValidator(Step.class.getSimpleName(), new StepValidator());
+    }
+
+
+
 }
