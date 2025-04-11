d2d02d3d0c782a7328d51478ddc0684045e4d8c2
fati <fatemesadatsedigh@gmail.com>
2025-04-11 11:13:30 +0330

    add createStep

--- src/todo/validator/StepService.java
+++ src/todo/validator/StepService.java
@@ -1,12 +1,24 @@
 package todo.validator;
 
 import db.Database;
+import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 import todo.entity.Step;
+import todo.service.StepValidator;
 
 public class StepService {
 
     static {
         Database.registerValidator(Step.class.getSimpleName(), new StepValidator());
+    }
+
+    public static int createStep(int taskId, String title)
+            throws InvalidEntityException, EntityNotFoundException {
+        TaskService.getTaskById(taskId);
+
+        Step step = new Step(title, taskId);
+        Database.add(step);
+        return step.id;
     }
 
 
