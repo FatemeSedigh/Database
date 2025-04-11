4e207952a614d53b85209bf994c204b38a642a3f
fati <fatemesadatsedigh@gmail.com>
2025-04-11 17:09:28 +0330

    add main for test cde

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,7 +3,7 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
-    public int age; // فیلد جدید age
+    public int age;
 
     public Human(String name, int age) {
         this.name = name;
